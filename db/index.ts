05239b558ff7d2a07df7dac7aff0120e3a5a3ca0
gctuzak <gctuzak@gmail.com>
2025-12-13 16:02:02 +0300

    chore: add db index

---
+++ db/index.ts
@@ -0,0 +1,24 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+import * as schema from './schema';
+import dotenv from 'dotenv';
+import dns from 'dns';
+
+if (dns.setDefaultResultOrder) {
+  dns.setDefaultResultOrder('ipv4first');
+}
+
+dotenv.config({ path: '.env.local' });
+
+const connectionString = process.env.DATABASE_URL;
+
+const isLocal =
+  !connectionString ||
+  connectionString.includes('localhost') ||
+  connectionString.includes('127.0.0.1');
+
+const client = postgres(
+  connectionString || 'postgres://postgres:postgres@localhost:5432/postgres',
+  isLocal ? {} : { ssl: 'require' }
+);
+export const db = drizzle(client, { schema });
